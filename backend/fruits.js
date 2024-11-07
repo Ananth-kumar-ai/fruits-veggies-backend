04bf4d5af1506090a68c13cfbc63081c3e479326
Ananth-kumar-ai <ananthkumarnalluri456@gmail.com>
2024-11-07 14:55:50 +0530

    Initial Commit

--- backend/fruits.js
+++ backend/fruits.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://ananthkumarnalluri456:<db_password>@cluster1.bhi29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
+  await mongoose.connect('mongodb+srv://ananthkumarnalluri456:MUNNA1234$@cluster1.bhi29.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1');
   // use await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test'); if your database has auth enabled
 }
 
